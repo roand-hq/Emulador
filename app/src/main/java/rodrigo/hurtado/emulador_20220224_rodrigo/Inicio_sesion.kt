9b97e2cafc1115933e15203f783f1d5786ba0441
Rodrigo Hurtado <20220224@ricaldone.edu.sv>
2024-04-28 21:44:55 -0600

    Hubiera agarrado eca

---
+++ app/src/main/java/rodrigo/hurtado/emulador_20220224_rodrigo/Inicio_sesion.kt
@@ -0,0 +1,27 @@
+package rodrigo.hurtado.emulador_20220224_rodrigo
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Inicio_sesion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_inicio_sesion)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnAcceder = findViewById<Button>(R.id.btnAcceder)
+        btnAcceder.setOnClickListener {
+            val intento = Intent(this, MainActivity::class.java)
+            startActivity(intento)
+        }
+    }
+}
---
+++ app/src/main/java/rodrigo/hurtado/emulador_20220224_rodrigo/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package rodrigo.hurtado.emulador_20220224_rodrigo.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
